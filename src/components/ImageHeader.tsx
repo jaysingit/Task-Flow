093e771b29e1c39671d4e5ef9b31cf2e39ae6b0c
Jayanth Mavilakkandy <jayanth1982@gmail.com>
2026-09-21 09:33:03 +0200

    Title updated

--- src/components/ImageHeader.tsx
+++ src/components/ImageHeader.tsx
@@ -5,7 +5,7 @@ import vitesvg from "../assets/vite.svg";
 export default function ImageHeader() {
   return (
     <>
-      <h1 className="h-10 font-bold text-center">TaskFlow</h1>
+      <h1 className="h-10 font-bold text-center">Task Flow</h1>
       <div className="flex items-center justify-center">
         <img className="w-20 h-28 object-contain" src={reactsvg} alt=""></img>
         <span className="mx-5"> + </span>
