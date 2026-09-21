a57de7a3253638e93cf8fe514ffd469355d36d07
Jayanth Mavilakkandy <jayanth1982@gmail.com>
2026-09-21 13:03:13 +0200

    Gradient style update

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ export default function App() {
   };
 
   return (
-    <div className="h-screen flex flex-col items-center bg-gradient-to-br from-indigo-500 via-purple-300 to-blue-400">
+    <div className="h-screen flex flex-col items-center bg-gradient-to-br from-indigo-700 via-white to-blue-400">
       <div className="w-87.5 pt-10">
         <ImageHeader></ImageHeader>
         <form onSubmit={handlerSubmit} className="mb-5">
