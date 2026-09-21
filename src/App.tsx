e592099234e652e21f11d1ebe031a7207906512a
Jayanth Mavilakkandy <jayanth1982@gmail.com>
2026-09-21 12:17:32 +0200

    Styles Update

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ export default function App() {
   };
 
   return (
-    <div className="h-screen flex flex-col items-center bg-linear-to-bl from-purple-500 via-blue-400 to-blue-500">
+    <div className="h-screen flex flex-col items-center bg-gradient-to-br from-indigo-500 via-purple-300 to-blue-400">
       <div className="w-87.5 pt-10">
         <ImageHeader></ImageHeader>
         <form onSubmit={handlerSubmit} className="mb-5">
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,8 +11,8 @@ export default function Input({ inputValue, setInputValue }: Input) {
       <input
         type="text"
         value={inputValue}
-        className="w-full p-2 rounded-sm mb-2 border border-black"
-        placeholder="Enter the task....."
+        className="w-full p-2 rounded-sm mb-2 border border-black text-black placeholder:text-black"
+        placeholder="Please enter the task here."
         onChange={(event) => setInputValue(event.target.value)}
       ></input>
     </>
